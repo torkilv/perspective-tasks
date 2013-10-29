989c46243eda51e777c3ec407938c75821f66bf9
Hans Magnus Inderberg <hinderberg@gmail.com>
2013-10-29 20:18:12 +0100

    Updated the usage of db and rest

--- dev.sh
+++ dev.sh
@@ -1,1 +1,1 @@
- SERVER_PORT=8888 SERVER_ALLOWED_ORIGIN="http://localhost:8000" DB_HOST="localhost" DB_PORT="28015" DATABASE_NAME="perspective" nodemon index.js
+ SERVER_PORT=8889 SERVER_ALLOWED_ORIGIN="http://localhost:8000" DB_HOST="localhost" DB_PORT="28015" DATABASE_NAME="perspective" nodemon index.js
